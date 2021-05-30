c890cdb46ebb5b18910d9166a394457046d8c48a
Dominic Elayda <nico@elayda.com>
2021-05-30 19:23:46 +0800

    alias: Add `rebrew`.

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,1 +1,2 @@
 alias unsign-gitkraken="codesign --remove-signature /Applications/GitKraken.app/Contents/Frameworks/GitKraken\ Helper\ \(Renderer\).app"
+alias rebrew="brew update && brew upgrade && brew cleanup"
